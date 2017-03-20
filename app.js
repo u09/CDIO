1586b95f85580b5ed337837c69e4bc7c614ee840
ahma0942 <ahmad.almajdi@yahoo.dk>
2017-03-20 09:58:48 +0100

    Highscore virker

--- app.js
+++ app.js
@@ -1,25 +1,55 @@
 var arDrone = require('ar-drone');
-var fs = require('fs');
 var http = require('http');
-var open = require('opencv');
+var cv = require('opencv');
+var im = arDrone.createClient().getPngStream();
 
-var pngStream = arDrone.createClient().getPngStream();
-pngStream.on('error',console.log).on('data',function(pngBuffer) {
-    var img = 'data:image/png;base64,'+new Buffer(pngBuffer).toString('base64');
-    var data = img.replace(/^data:image\/\w+;base64,/, "");
-    var buf = new Buffer(data, 'base64');
-    fs.writeFile('image.png', buf);
+try {
+  var camera = new cv.VideoCapture(0);
+  var window = new cv.NamedWindow('Video', 0);
+  setInterval(function() {
+    camera.read(function(err, im) {
+      if (err) throw err;
+      console.log(im.size())
+      if (im.size()[0] > 0 && im.size()[1] > 0){
+		  window.show(im);
+      }
+      window.blockingWaitKey(0, 50);
+    });
+  }, 20);
+} catch (e){
+  console.log("Couldn't start camera:", e)
+}
+/*
+
+
+var lastPng;
+pngStream.on('error',console.log).on('data',function(pngBuffer){
+	cv.readImage(pngBuffer,function(err,im){
+		if (err) throw err;
+		if (im.width() < 1 || im.height() < 1) throw new Error('Image has no size');
+
+		im.detectObject("haarcascade_frontalface_alt.xml", {}, function(err, faces){
+			if(err) throw err;
+			
+			for(var i = 0; i < faces.length; i++){
+				var face = faces[i];
+				im.ellipse(face.x+face.width/2,face.y+face.height/2,face.width/2,face.height/2);
+			}
+			
+			im.save('image2.png');
+			console.log('Image saved to image2.png');
+		});
+	});
+	lastPng = 'data:image/png;base64,'+new Buffer(pngBuffer).toString('base64');
 });
 
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html' });
-//	res.write('<html><head><script> setInterval(function(){ document.getElementById("image").src="./image.png"; },1000); </script></head><body>');
-    res.write('<img id="image" src="image.png"/>');
-//	res.end('</body></html>');
+    res.write('<img id="image" src="'+lastPng+'"/>');
     res.end();
 });
-server.listen(8080, function() {
-    console.log('Serving latest png on port 80 ...');
-});
 
-var cv = require('/node_modules/opencv/lib/opencv');
+server.listen(80,function(){
+	console.log('Serving latest png on port 80 ...');
+});
+*/
--- opencv.js
+++ opencv.js
@@ -1,27 +1,29 @@
 var arDrone = require('ar-drone');
-var cv = require('node-opencv/lib/opencv');
+var cv = require('opencv');
 var fs = require('fs');
 var http = require('http');
 var pngStream = arDrone.createClient().getPngStream();
-
+/*
 pngStream.on('error',console.log).on('data',function(pngBuffer) {
     var img = 'data:image/png;base64,'+new Buffer(pngBuffer).toString('base64');
     var data = img.replace(/^data:image\/\w+;base64,/,"");
     var buf = new Buffer(data, 'base64');
     fs.writeFile('image.png', buf);
 });
+*/
+cv.readImage("image.png", function(err, im){
+  if (err) throw err;
+  if (im.width() < 1 || im.height() < 1) throw new Error('Image has no size');
 
-cv.readImage("image.png",function(err, im){
-	if (err) throw err;
-	if (im.width() < 1 || im.height() < 1) throw new Error('Image has no size');
+  im.detectObject("../data/haarcascade_frontalface_alt.xml", {}, function(err, faces){
+    if (err) throw err;
 
-	im.detectObject("../data/haarcascade_frontalface_alt.xml", {}, function(err, faces){
-		if (err) throw err;
-		for (var i = 0; i < faces.length; i++){
-			var face = faces[i];
-			im.ellipse(face.x + face.width / 2, face.y + face.height / 2, face.width / 2, face.height / 2);
-		}
-		im.save('image2.png');
-		console.log('Image saved to image2.png');
-	});
+    for (var i = 0; i < faces.length; i++){
+      var face = faces[i];
+      im.ellipse(face.x + face.width / 2, face.y + face.height / 2, face.width / 2, face.height / 2);
+    }
+
+    im.save('./tmp/face-detection.png');
+    console.log('Image saved to ./tmp/face-detection.png');
+  });
 });
