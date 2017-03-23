d1f8a6d35245f4b128597252225123ea7aef93f0
u09 <u_09@live.dk>
2017-03-23 10:42:29 +0100

    Rettet fejl

--- app-pc-camera.js
+++ app-pc-camera.js
@@ -1,24 +1,17 @@
 var cv = require('opencv');
 
 try {
-    var camera = new cv.VideoStream(0);
+    var camera = new cv.VideoCapture(0);
     var window = new cv.NamedWindow('Video', 0);
 
     setInterval(function() {
         camera.read(function(err, im) {
-            console.log("test1");
             if (err) throw err;
-            console.log("test2");
             console.log(im.size());
-            console.log("test3");
             if (im.size()[0] > 0 && im.size()[1] > 0) {
-                console.log("test4");
                 window.show(im);
-                console.log("test5");
             }
-            console.log("test6");
             window.blockingWaitKey(0, 50);
-            console.log("test7");
         });
     }, 20);
 
