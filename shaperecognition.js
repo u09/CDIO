5d86f6e4acd1178ff5665ac07ac31b7da6f81894
ahma0942 <ahmad.almajdi@yahoo.dk>
2017-03-29 13:20:49 +0200

    asd

--- shaperecognition.js
+++ shaperecognition.js
@@ -2,27 +2,14 @@ var cv = require('opencv');
 var video = new cv.VideoCapture(0);
 var win = new cv.NamedWindow('Video',0);
 
-const WHITE = [255, 255, 255];
-let contours = img.contours();
-let largestContourImg;
-let largestArea = 0;
-let largestAreaIndex;
-
-for (let i = 0; i < contours.size(); i++) {
-	if (contours.area(i) > largestArea) {
-		largestArea = contours.area(i);
-		largestAreaIndex = i;
-	}
-}
-
-largestContourImg.drawContour(contours, largestAreaIndex, GREEN, thickness, lineType);
-
 setInterval(function() {
 	video.read(function(err, im) {
 		if (err) throw err;
 		console.log(im.size())
 		if (im.size()[0] > 0 && im.size()[1] > 0){
-//			im.convertHSVscale();
+			im.convertHSVscale();
+			im.inRange(hsv_image, cv::Scalar(0, 100, 100), cv::Scalar(10, 255, 255), lower_red_hue_range);
+			cv::inRange(hsv_image, cv::Scalar(160, 100, 100), cv::Scalar(179, 255, 255), upper_red_hue_range);
 			win.show(im);
 		}
 		win.blockingWaitKey(0, 50);
