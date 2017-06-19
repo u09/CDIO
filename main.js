21b78141c5c0cb1a9d6c94d7eab6d097c4f86908
ahma0942 <ahmad.almajdi@yahoo.dk>
2017-06-19 09:01:57 +0200

    AI

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
-// Detects triangles and quadrilaterals
-var cv = require('opencv');
 var arDrone = require('ar-drone');
+var cv = require('opencv');
 var client=arDrone.createClient();
 
-// (B)lue, (G)reen, (R)ed
 var lower_threshold = [100, 100, 255];
 var upper_threshold = [0, 0, 150];
 
-var s = new cv.ImageStream()
+var s = new cv.ImageStream();
 var check=false;
 var center=false;
 var stop=false;
 var ar=50;
 var c,x,y,r,w,h;
+var delay=250
 
+client.createRepl();
+
+console.log("Igang med Takeoff");
 client.takeoff();
-client.calibrate(0);
+client.after(5000,function(){
+	console.log("Takeoff færdig.");
+	console.log("Igang med Calibrate");
+	client.calibrate(0);
+});
+client.after(7000,function(){
+	console.log("Calibrate færdig.");
+	console.log("Igang med at sætte den ønskede højde.");
+	client.up(0.5)
+});
+client.after(2000,function(){
+	console.log("Højde sat.");
+	this.stop();
+});
 
-setInterval(function(){
-	check=true;
-},5000);
+client.after(2000,function(){
+	console.log("Starter billedgenkendelse");
+	setInterval(function(){
+		check=true;
+	},5000);
+});
 
 s.on('data', function(im){
 	if(check && !stop)
@@ -29,23 +47,24 @@ s.on('data', function(im){
 		h=im.height();
 		
 		if (w < 1 || h < 1) throw new Error('Image has no size');
+		
 //		im.inRange(upper_threshold,lower_threshold);
 		im.convertGrayscale();
-		c = im.houghCircles(1.1, 100);
+		c = im.houghCircles(1.2, 100);
 		
 		if(c.length==1)
 		{
 			x=Math.round(c[0][0]);
 			y=Math.round(c[0][1]);
 			r=Math.round(c[0][2]);
-			
+			console.log(x+" "+y+" "+r);
 			if(c.length==1 && x>w/2-ar/2 && x<w/2+ar/2 && y>h/2-ar/2 && y<h/2+ar/2) center=true;
 			else center=false;
 			console.log(c.length+"\t"+im.width()+"\t"+im.height()+"\t"+(center?"true":"false"));
 			
 			if(center) {
 				client.front(0.1);
-				client.after(2000,function(){
+				client.after(3500,function(){
 					this.stop();
 				});
 //				stop=true;
@@ -53,29 +72,29 @@ s.on('data', function(im){
 			else {
 				if(x<w/2-ar/2)
 				{
-					client.counterClockwise(0.1);
-					client.after(500,function(){
+					client.left(0.1);
+					client.after(delay,function(){
 						this.stop();
 					});
 				}
 				else if(x>w/2+ar/2)
 				{
-					client.clockwise(0.1);
-					client.after(500,function(){
+					client.right(0.1);
+					client.after(delay,function(){
 						this.stop();
 					});
 				}
 				else if(y<h/2-ar/2)
 				{
 					client.up(0.1);
-					client.after(500,function(){
+					client.after(delay,function(){
 						this.stop();
 					});
 				}
 				else if(y>h/2+ar/2)
 				{
 					client.down(0.1);
-					client.after(500,function(){
+					client.after(delay,function(){
 						this.stop();
 					});
 				}
@@ -86,4 +105,3 @@ s.on('data', function(im){
 });
 
 client.getPngStream().pipe(s);
-client.createRepl();
